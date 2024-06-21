1e16aba569480a1ae4f1b75afdde7156316892ec
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-06-21 14:38:12 -0700

    Add attributes to Rabbit
    - Add json file
    - Read data from json file and initialize with data
    - Tested rabbit creation with function

--- rabbit.py
+++ rabbit.py
@@ -1,3 +1,26 @@
+import json
+
+
 class Rabbit:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, data) -> None:
+        self.age = data['age']
+        self.weight = data['weight']
+        self.calories = data['calories']
+        self.energy = data['energy']
+        self.state = None
+
+
+with open('rabbit_data.json', 'r') as file:
+    data = json.load(file)
+
+
+def test_rabbit_creation():
+    rabbit = Rabbit(data)
+    print(f'Rabbit age: {rabbit.age}')
+    print(f'Rabbit weight: {rabbit.weight}')
+    print(f'Rabbit calories: {rabbit.calories}')
+    print(f'Rabbit energy: {rabbit.energy}')
+    print(f'Rabbit state: {rabbit.state}')
+
+
+test_rabbit_creation()
