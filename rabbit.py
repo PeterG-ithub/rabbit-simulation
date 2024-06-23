9ed8000934a93fbdbe5cb9a16be056b2721a7a88
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-06-22 21:02:04 -0700

    Add functions
    - Add eat function
    - Add grow function
    - Add sleep function

--- rabbit.py
+++ rabbit.py
@@ -3,11 +3,20 @@ import json
 
 class Rabbit:
     def __init__(self, data) -> None:
-        self.age = data['age']
-        self.weight = data['weight']
+        self.age = data['age']  # Base on days
+        self.weight = data['weight']  # Base on grams
         self.calories = data['calories']
         self.energy = data['energy']
         self.state = None
+
+    def eat(self, calories):
+        self.calories += calories
+
+    def grow(self):  # Grow base on excess calories when sleeping
+        pass
+
+    def sleep(self):  # Increase energy 
+        pass
 
 
 with open('rabbit_data.json', 'r') as file:
