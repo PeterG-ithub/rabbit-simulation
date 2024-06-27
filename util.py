491cc6770be57eb54c7bb05bd98ce05a467f84c7
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-06-26 22:35:52 -0700

    Add guard condition to lerp function

--- util.py
+++ util.py
@@ -1,2 +1,8 @@
-def lerp(a, b, t):
-    return (a + t * (b - a))
+def lerp(a, b, t):  # lerp(PL, PU, t) where: PL = lower probability, PU = Upper Probablity
+    num = (b + t * (a - b))
+    num = round(num, 2)
+    if num < a:
+        num = a
+    elif num > b:
+        num = b
+    return num
