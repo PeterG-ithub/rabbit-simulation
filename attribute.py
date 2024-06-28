10a1c3c77375999fe1ceeafeed48a11bf4c9008e
PeterG <98351061+PeterG-ithub@users.noreply.github.com>
2024-06-27 22:39:27 -0700

    Create Attribute and Chance class
    - Both class have name, value, upper, lower as attributes

---
+++ attribute.py
@@ -0,0 +1,6 @@
+class Attribute:
+    def __init__(self, name="", data={}) -> None:
+        self.name = name
+        self.value = data['value']
+        self.upper = data['upper']
+        self.lower = data['lower']
